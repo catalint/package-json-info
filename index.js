379796efb3049a2436399c10640e1a967b0fe81f
Cătălin Tănăsescu <catalint@gmail.com>
2020-06-18 01:35:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 const run = async () => {
    
-    const packagePath = path.join((process.env.GITHUB_WORKSPACE, process.env['INPUT_PATH'] || 'package.json'));
+    const packagePath = path.join(process.env.GITHUB_WORKSPACE, process.env['INPUT_PATH'] || 'package.json');
     console.log("Trying to read:",packagePath);
+    
    
     const package = require(packagePath);
     setOutput('version', package.version);
 }
 
 run().catch(error => {
+   console.log('env is',JSON.stringify(process.env,null,2);
     setFailed(error.message);
 });
